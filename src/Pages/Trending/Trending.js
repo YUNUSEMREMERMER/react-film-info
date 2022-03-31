ac95ce86cf55720087846ffb73bd048c0b3907a0
YUNUS EMRE MERMER <yunusemremermer57@gmail.com>
2022-03-31 12:05:36 +0300

    31.03.22

--- src/Pages/Trending/Trending.js
+++ src/Pages/Trending/Trending.js
@@ -1,25 +1,36 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import CustomPagination from '../../components/Pagination/CustomPagination';
 import SingleContent from '../../components/SingleContent/SingleContent'
 import "./Trending.css"
 
 function Trending() {
+
+  const [page, setPage] = useState(1);
+  const [content, setContent] = useState([]);
+
+  const fetchTrending = async () => {
+    const { data } = await axios.get(`https://api.themoviedb.org/3/trending/all/day?api_key=${process.env.REACT_APP_API_KEY}&page=${page}`);
+
+    setContent(data.results);
+  };
+
+  useEffect(() => {
+    fetchTrending();
+  }, [page]);
+
+
   return (
     <div>
-        <span className='pageTitle'>Trending</span>
-        <div className='trending'>
-          <SingleContent key={1} id={1} poster="https://www.movienewz.com/img/films/poster-holder.jpg" title="film" date="11/11/11" media_type="movie" />
-          <SingleContent key={2} id={2} poster="https://www.movienewz.com/img/films/poster-holder.jpg" title="film" date="11/11/11" media_type="movie" />
-          <SingleContent key={3} id={3} poster="https://www.movienewz.com/img/films/poster-holder.jpg" title="film" date="11/11/11" media_type="movie" />
-          <SingleContent key={4} id={4} poster="https://www.movienewz.com/img/films/poster-holder.jpg" title="film" date="11/11/11" media_type="movie" />
-          <SingleContent key={5} id={5} poster="https://www.movienewz.com/img/films/poster-holder.jpg" title="film" date="11/11/11" media_type="movie" />
-          <SingleContent key={1} id={1} poster="https://www.movienewz.com/img/films/poster-holder.jpg" title="film" date="11/11/11" media_type="movie" />
-          <SingleContent key={2} id={2} poster="https://www.movienewz.com/img/films/poster-holder.jpg" title="film" date="11/11/11" media_type="movie" />
-          <SingleContent key={3} id={3} poster="https://www.movienewz.com/img/films/poster-holder.jpg" title="film" date="11/11/11" media_type="movie" />
-          <SingleContent key={4} id={4} poster="https://www.movienewz.com/img/films/poster-holder.jpg" title="film" date="11/11/11" media_type="movie" />
-          <SingleContent key={5} id={5} poster="https://www.movienewz.com/img/films/poster-holder.jpg" title="film" date="11/11/11" media_type="movie" />
-
-
-        </div>
+      <span className='pageTitle'>Trending</span>
+      <div className='trending'>
+        {
+          content && content.map((c) => (
+            <SingleContent key={c.id} id={c.id} poster={c.poster_path} title={c.title || c.name} date={c.first_air_date || c.release_date} media_type={c.media_type} vote_average={c.vote_average} />
+          ))
+        }
+      </div>
+      <CustomPagination setPage={setPage} />
     </div>
   )
 }
