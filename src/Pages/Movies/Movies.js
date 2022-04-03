734bd19cf4350825ef02cf4542d55ec9c3af197c
YUNUS EMRE MERMER <yunusemremermer57@gmail.com>
2022-04-03 16:11:28 +0300

    03.04.22

--- src/Pages/Movies/Movies.js
+++ src/Pages/Movies/Movies.js
@@ -2,6 +2,8 @@ import axios from 'axios';
 import React,{useState, useEffect} from 'react'
 import CustomPagination from '../../components/Pagination/CustomPagination';
 import SingleContent from '../../components/SingleContent/SingleContent'
+import Genres from '../../components/Genres'
+import useGenre from "../../hooks/useGenre";
 import "./Movies.css"
 
 function Movies() {
@@ -9,9 +11,12 @@ function Movies() {
   const [page, setPage] = useState(1);
   const [content, setContent] = useState([]);
   const [numOfPages, setNumOfPages] = useState();
-
+  const [selectedGenres, setSelectedGenres] = useState([]);
+  const [genres, setGenres] = useState([]);
+  const genreforURL = useGenre(selectedGenres);
+ 
   const fetchMovies = async() => {
-    const { data } = await axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}`);
+    const { data } = await axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}&with_genres=${genreforURL}`);
     setContent(data.results);
     setNumOfPages(data.total_pages);  
 
@@ -20,11 +25,18 @@ function Movies() {
   useEffect(() => {
     fetchMovies();
     //eslint-disable-next-line
-  }, [page]);
+  }, [page,genreforURL]);
 
   return (
     <div>
         <span className='pageTitle'>Movies</span>
+        <Genres
+          type="movie"
+          selectedGenres={selectedGenres} setSelectedGenres = {setSelectedGenres}
+          genres = {genres} setGenres = {setGenres}
+          setPage = {setPage}
+
+        />
         <div className='movies'>
         {
           content && content.map((c) => (
@@ -37,7 +49,7 @@ function Movies() {
           ))
         }
       </div>
-      <CustomPagination setPage={setPage} numOfPages={500}/>
+      <CustomPagination setPage={setPage} numOfPages={numOfPages > 500 ? 500 : numOfPages}/>
     </div>
   )
 }
--- src/Pages/Search/Search.js
+++ src/Pages/Search/Search.js
@@ -1,9 +1,43 @@
+
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import {  Button, TextField } from '@mui/material';
 import React from 'react'
+import SearchIcon from '@mui/icons-material/Search';
 
 function Search() {
+
+
+  const darkTheme = createTheme({
+    palette: {
+      mode: 'dark',
+    },
+  });
+
+
   return (
     <div>
-        <span className='pageTitle'>Search</span>
+        <ThemeProvider theme={darkTheme}>
+          <div style={{display: "flex", margin:"15px 0"}}>
+            <TextField 
+            style={{flex:1}}
+            className="searchbox"
+            label="search"
+            variant='filled'
+            //onChange={(e) => setSearchText(e.target.value)}
+          />
+
+            <Button variant='contained' style={{marginLeft:10}}>
+              <SearchIcon />
+            </Button>
+          </div>
+          
+          
+
+        </ThemeProvider>
+        
+        
+          
+        
     </div>
   )
 }
