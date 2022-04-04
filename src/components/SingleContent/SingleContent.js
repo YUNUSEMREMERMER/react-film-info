e70184a193bc85e59b820435d8949905675f3290
YUNUS EMRE MERMER <yunusemremermer57@gmail.com>
2022-04-04 16:33:25 +0300

    04.04.22 - 2

--- src/components/SingleContent/SingleContent.js
+++ src/components/SingleContent/SingleContent.js
@@ -2,10 +2,11 @@ import { Badge } from '@mui/material'
 import React from 'react'
 import "./SingleContent.css"
 import { img_300, unavailable } from "../../config/config";
+import ContentModal from "../ContentModal/ContentModal";
 
 function SingleContent({id, poster, title, date, media_type, vote_average}) {
   return (
-    <div className='media'>
+    <ContentModal media_type={media_type} id={id}>
         <Badge badgeContent={vote_average} color={vote_average > 6 ? "primary" : "secondary"} />
         <img className='poster' src={poster ? `${img_300}/${poster}` : unavailable} alt="title" />
         <b className='title'>{title}</b>
@@ -13,7 +14,7 @@ function SingleContent({id, poster, title, date, media_type, vote_average}) {
             {media_type === "tv" ? "TV Series" : "Movie"}
             <span className='subTitle'>{date}</span>
         </span>
-    </div>
+    </ContentModal>
   )
 }
 
